fd2ffced5a5371523941db2f2a3a54bce2b014e9
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-02 03:27:10 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -9,7 +9,11 @@ def state_log(star_date)
   "Captain's Log, star date #{generate_star_date}."
 end
 
+def crew_greeting
+"Hello crew"
+end
+
 def engage
-  puts state_log(date)
-  date = generate_star_date
+  puts state_log
+  puts crew_greeting
 end
